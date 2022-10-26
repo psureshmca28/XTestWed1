ad2c0ac4977be8ffc68ff44386f106af57d0304c
psureshmca28 <115546208+psureshmca28@users.noreply.github.com>
2022-10-26 11:50:50 +0530

    second commit from GIT

--- src/Test1.java
+++ src/Test1.java
@@ -3,7 +3,8 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Test1 added");
+		System.out.println("Test1 added");
+		System.out.println("Test2 added");
 	}
 
 }
